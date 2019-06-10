dbd4cbe446142a1c7c1e0c41c833b4e0aae716c5
Shubham Kumar <shubham.kumar1739@gmail.com>
2019-06-10 14:05:38 +0530

    Code to remove stopwords

---
+++ smobile.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+from nltk.corpus import stopwords
+import pandas as pd
+import numpy as np
+import string
+from nltk.tokenize import word_tokenize
+
+stopwords_set=set(stopwords.words("english"))
+
+data=pd.read_csv(r"Smobile.csv")
+data["Reviews"]=data["Reviews"].replace(np.nan,"NA")
+print("Start Tokenization ... ")
+tokens=[word_tokenize(item) for item in list(data["Reviews"])]
+print("Tokenization Complete ... ")
+final_tokens=[[item for item in text if item not in string.punctuation and item not in stopwords_set] for text in tokens]
+print("Stopwords Removed ... ")
+final_words=[" ".join(token) for token in final_tokens]
+print("Words Joined ... ")
+data["Reviews"]=final_words
+print(data["Reviews"].iloc[0])
